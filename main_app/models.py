7b4a3a7238d13765864b9b8a687d50a1513927c4
Anton Zvontsov <azvontsov@gmail.com>
2022-03-02 14:39:28 -0500

    adds user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from string import digits
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 CHARGES = (
     ('C', 'Charge'),
@@ -23,8 +24,8 @@ class Copter(models.Model):
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
     kits = models.ManyToManyField(Kit)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
